53103795921bc8ac6f7e51a4a2de289d7fce4c29
Aliaksei <brel.2000@mail.ru>
2021-04-07 18:46:13 +0300

    unit: create autoForm, opening

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@ const burger = document.querySelector('.header__close'),
     list = document.querySelector('.header__list'),
     body = document.getElementsByTagName('body')[0];
 
-
 burger.addEventListener('click', () => {
     menu.classList.toggle('active');
     burger.classList.toggle('active');
     list.classList.toggle('active');
+    body.classList.toggle('luck');
+})
+
+const btnSubmit = document.querySelector('.submit-link'),
+    formAuto = document.querySelector('.form__auto');
+
+btnSubmit.addEventListener('click', () => {
+    formAuto.classList.toggle('hidden');
     body.classList.toggle('luck');
 })
